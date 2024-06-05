9ba3a75faa21395efdb43a6ba75d5a6104f421ed
Shirayaki <167289184+Shirayaki@users.noreply.github.com>
2024-06-05 15:57:25 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//JavaScript
+// console.log('Hello world!');
+// console.log("console.log")
+
+$(function(){
+    $('.bxslider').bxSlider({
+        mode:'fade',
+        captions:true,
+        auto:true,
+        
+    });
+});
+  
